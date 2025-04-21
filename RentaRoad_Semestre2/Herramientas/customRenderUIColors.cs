e1361452f1b3f460a3e5d884fea8f4dd86a1b5c2
ai-zac <isaacjdv31@gmail.com>
2025-04-20 23:22:20 -0600

    Añadido carpeta Herramientas, clase de uso general de cambio de color de opciones de menustrip, cambio de nombre de formulario home y eliminación de formularios sin uso

---
+++ RentaRoad_Semestre2/Herramientas/customRenderUIColors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentaRoad_Semestre3.Herramientas
+{
+    public class customRenderUIColors : ToolStripProfessionalRenderer
+    {
+        public customRenderUIColors() : base(new customColorsRender()) { }
+
+    }
+    public class customColorsRender : ProfessionalColorTable
+    {
+        public override Color ToolStripDropDownBackground 
+        {
+            get { return Color.FromArgb(56, 62, 70); }
+        }
+        public override Color MenuItemSelectedGradientBegin
+        {
+            get { return Color.DimGray; }
+        }
+        public override Color MenuItemSelectedGradientEnd
+        {
+            get { return Color.DimGray; }
+        }
+        public override Color MenuItemPressedGradientBegin
+        {
+            get { return Color.DimGray; }
+        }
+        public override Color MenuItemPressedGradientMiddle
+        {
+            get { return Color.DimGray; }
+        }
+        public override Color MenuItemPressedGradientEnd
+        {
+            get { return Color.DimGray; }
+        }
+    }
+}

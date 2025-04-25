73b48d04b98afecbfce9180ac65cada360de0e19
Steven2018A <stiven20cru2z@gmail.com>
2025-04-25 17:06:45 -0600

    avances

--- RentaRoad_Semestre2/CapaPresentacion/frm_RecuperarContraseña.Designer.cs
+++ RentaRoad_Semestre2/CapaPresentacion/frm_RecuperarContraseña.Designer.cs
@@ -29,99 +29,244 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_RecuperarContraseña));
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox23 = new System.Windows.Forms.TextBox();
-            this.label26 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox3.SuspendLayout();
-            this.SuspendLayout();
+            groupBox3 = new GroupBox();
+            btnCorreo = new Button();
+            txtCorreo = new TextBox();
+            label26 = new Label();
+            label1 = new Label();
+            panelCorreo = new Panel();
+            panelToken = new Panel();
+            groupBox1 = new GroupBox();
+            btnToken = new Button();
+            txtToken = new TextBox();
+            label2 = new Label();
+            panelContraseña = new Panel();
+            groupBox2 = new GroupBox();
+            btnContraseña = new Button();
+            txtContraseña = new TextBox();
+            label3 = new Label();
+            groupBox3.SuspendLayout();
+            panelCorreo.SuspendLayout();
+            panelToken.SuspendLayout();
+            groupBox1.SuspendLayout();
+            panelContraseña.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.button3);
-            this.groupBox3.Controls.Add(this.textBox23);
-            this.groupBox3.Controls.Add(this.label26);
-            this.groupBox3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(40, 70);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox3.Size = new System.Drawing.Size(330, 112);
-            this.groupBox3.TabIndex = 88;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Ingrese su correo electrónico";
+            groupBox3.Controls.Add(btnCorreo);
+            groupBox3.Controls.Add(txtCorreo);
+            groupBox3.Controls.Add(label26);
+            groupBox3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox3.Location = new Point(48, 74);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(440, 146);
+            groupBox3.TabIndex = 88;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Ingrese su correo electrónico";
             // 
-            // button3
+            // btnCorreo
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
-            this.button3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.Location = new System.Drawing.Point(13, 52);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 30);
-            this.button3.TabIndex = 76;
-            this.button3.Text = "Enviar";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            btnCorreo.BackColor = Color.FromArgb(0, 80, 200);
+            btnCorreo.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCorreo.ForeColor = Color.White;
+            btnCorreo.Image = (Image)resources.GetObject("btnCorreo.Image");
+            btnCorreo.ImageAlign = ContentAlignment.MiddleRight;
+            btnCorreo.Location = new Point(17, 80);
+            btnCorreo.Name = "btnCorreo";
+            btnCorreo.Size = new Size(120, 46);
+            btnCorreo.TabIndex = 76;
+            btnCorreo.Text = "Enviar";
+            btnCorreo.TextAlign = ContentAlignment.MiddleLeft;
+            btnCorreo.UseVisualStyleBackColor = false;
+            btnCorreo.Click += btnCorreo_Click;
             // 
-            // textBox23
+            // txtCorreo
             // 
-            this.textBox23.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox23.Location = new System.Drawing.Point(13, 26);
-            this.textBox23.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox23.Name = "textBox23";
-            this.textBox23.Size = new System.Drawing.Size(297, 22);
-            this.textBox23.TabIndex = 67;
+            txtCorreo.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtCorreo.Location = new Point(17, 40);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(395, 26);
+            txtCorreo.TabIndex = 67;
             // 
             // label26
             // 
-            this.label26.AutoSize = true;
-            this.label26.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label26.ForeColor = System.Drawing.Color.Black;
-            this.label26.Location = new System.Drawing.Point(9, 29);
-            this.label26.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label26.Name = "label26";
-            this.label26.Size = new System.Drawing.Size(0, 16);
-            this.label26.TabIndex = 66;
+            label26.AutoSize = true;
+            label26.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label26.ForeColor = Color.Black;
+            label26.Location = new Point(12, 45);
+            label26.Name = "label26";
+            label26.Size = new Size(0, 19);
+            label26.TabIndex = 66;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(34, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(307, 32);
-            this.label1.TabIndex = 87;
-            this.label1.Text = "Recuperar contraseña";
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(385, 40);
+            label1.TabIndex = 87;
+            label1.Text = "Recuperar contraseña";
             // 
-            // RecuperarContraseña
+            // panelCorreo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(406, 221);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.label1);
-            this.Name = "RecuperarContraseña";
-            this.Text = "RecuperarContraseñá";
-            this.Load += new System.EventHandler(this.RecuperarContraseñá_Load);
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            panelCorreo.Controls.Add(groupBox3);
+            panelCorreo.Location = new Point(1, 52);
+            panelCorreo.Name = "panelCorreo";
+            panelCorreo.Size = new Size(540, 288);
+            panelCorreo.TabIndex = 89;
+            // 
+            // panelToken
+            // 
+            panelToken.Controls.Add(groupBox1);
+            panelToken.Location = new Point(0, 49);
+            panelToken.Name = "panelToken";
+            panelToken.Size = new Size(540, 288);
+            panelToken.TabIndex = 90;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnToken);
+            groupBox1.Controls.Add(txtToken);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(48, 74);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(440, 146);
+            groupBox1.TabIndex = 88;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingrese el token de recuperación";
+            // 
+            // btnToken
+            // 
+            btnToken.BackColor = Color.FromArgb(0, 80, 200);
+            btnToken.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnToken.ForeColor = Color.White;
+            btnToken.ImageAlign = ContentAlignment.MiddleRight;
+            btnToken.Location = new Point(17, 80);
+            btnToken.Name = "btnToken";
+            btnToken.Size = new Size(120, 46);
+            btnToken.TabIndex = 76;
+            btnToken.Text = "Validar";
+            btnToken.UseVisualStyleBackColor = false;
+            btnToken.Click += btnToken_Click;
+            // 
+            // txtToken
+            // 
+            txtToken.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtToken.Location = new Point(17, 40);
+            txtToken.Name = "txtToken";
+            txtToken.Size = new Size(395, 26);
+            txtToken.TabIndex = 67;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(12, 45);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 19);
+            label2.TabIndex = 66;
+            // 
+            // panelContraseña
+            // 
+            panelContraseña.Controls.Add(groupBox2);
+            panelContraseña.Location = new Point(0, 52);
+            panelContraseña.Name = "panelContraseña";
+            panelContraseña.Size = new Size(540, 288);
+            panelContraseña.TabIndex = 90;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnContraseña);
+            groupBox2.Controls.Add(txtContraseña);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(48, 74);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(440, 146);
+            groupBox2.TabIndex = 88;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Ingrese su nueva contraseña";
+            // 
+            // btnContraseña
+            // 
+            btnContraseña.BackColor = Color.FromArgb(0, 80, 200);
+            btnContraseña.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnContraseña.ForeColor = Color.White;
+            btnContraseña.ImageAlign = ContentAlignment.MiddleRight;
+            btnContraseña.Location = new Point(17, 80);
+            btnContraseña.Name = "btnContraseña";
+            btnContraseña.Size = new Size(120, 46);
+            btnContraseña.TabIndex = 76;
+            btnContraseña.Text = "Guardar";
+            btnContraseña.UseVisualStyleBackColor = false;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtContraseña.Location = new Point(17, 40);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(395, 26);
+            txtContraseña.TabIndex = 67;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(12, 45);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 19);
+            label3.TabIndex = 66;
+            // 
+            // frm_RecuperarContraseña
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(541, 340);
+            Controls.Add(panelCorreo);
+            Controls.Add(panelToken);
+            Controls.Add(panelContraseña);
+            Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "frm_RecuperarContraseña";
+            Text = "RecuperarContraseñá";
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            panelCorreo.ResumeLayout(false);
+            panelToken.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            panelContraseña.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox23;
+        private System.Windows.Forms.Button btnCorreo;
+        private System.Windows.Forms.TextBox txtCorreo;
         private System.Windows.Forms.Label label26;
         private System.Windows.Forms.Label label1;
+        private Panel panelCorreo;
+        private Panel panelToken;
+        private GroupBox groupBox1;
+        private Button btnToken;
+        private TextBox txtToken;
+        private Label label2;
+        private Panel panelContraseña;
+        private GroupBox groupBox2;
+        private Button btnContraseña;
+        private TextBox txtContraseña;
+        private Label label3;
     }
 }
--- RentaRoad_Semestre2/CapaPresentacion/frm_RecuperarContraseña.cs
+++ RentaRoad_Semestre2/CapaPresentacion/frm_RecuperarContraseña.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using RentaRoad_Semestre3.CapaDatos.Modelos;
+using RentaRoad_Semestre3.CapaDatos.Repositorios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,24 +16,91 @@ namespace RentaRoad_Semestre3.CapaPresentacion
 {
     public partial class frm_RecuperarContraseña : Form
     {
+        private UsuariosService _usuarioService;
+
         public frm_RecuperarContraseña()
         {
             InitializeComponent();
+
+            // Configurar el cursor de la BD
+            RentaRoadDbContext context = new RentaRoadDbContext();
+            UsuarioRepositorio repo = new UsuarioRepositorio(context);
+            _usuarioService = new UsuariosService(repo);
         }
 
-        private void RecuperarContraseñá_Load(object sender, EventArgs e)
+        private void btnCorreo_Click(object sender, EventArgs e)
+        {
+            if (txtCorreo.Text == null) return;
+
+            Usuario? usuaExistente = _usuarioService.ObtenerTodos()
+                                    .FirstOrDefault(u => u.CorreoUsuario == txtCorreo.Text);
+            if (usuaExistente == null)
+            {
+                MessageBox.Show("El correo no existe en la base de datos");
+                return;
+            }
+
+            string tokenRecuperacion = Guid.NewGuid().ToString();
+
+            usuaExistente.TokenRecuperacion = tokenRecuperacion;
+            usuaExistente.FechaLimiteRecuperacion = DateTime.Now.AddMinutes(5);
+            _usuarioService.ActualizarUsuario(usuaExistente);
+
+            if (enviarCorreo(txtCorreo.Text, tokenRecuperacion))
+            {
+                panelCorreo.Visible = false;
+                return;
+            }
+
+        }
+
+        private void btnToken_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private bool enviarCorreo(string correoDestino, string token)
         {
+            SmtpClient stmp = null;
+            try
+            {
+                stmp = new SmtpClient("smtp.zoho.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential("rentaroad_unan@zohomail.com", "6q2ei6eMCUfa"),
+                    EnableSsl = true,
+                    UseDefaultCredentials = false
+                };
+            }
 
-        }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hubo un error al configurar el cliente");
+                return false;
+            }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Hide();
+            string asuntoCorreo = "Recuperación de contraseña";
+            string cuerpoCorreo = $"Tu código de recuperación: {token}";
+
+            try
+            { 
+                var mensajeCorreo = new MailMessage
+                {
+                    From = new MailAddress("rentaroad_unan@zohomail.com"),
+                    Subject = asuntoCorreo,
+                    Body = cuerpoCorreo,
+                    IsBodyHtml = false  
+                };
+
+                mensajeCorreo.To.Add(correoDestino);
+                stmp.Send(mensajeCorreo);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Hubo un error al procesar el correo");
+                return false;
+            }
+            return true;
         }
     }
 }
